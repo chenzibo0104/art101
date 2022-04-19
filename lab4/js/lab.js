aaa203d32defb6b7fd7afaef717bf4996497b8e3
Zibo Chen <chenzibo0104@gmail.com>
2022-04-18 17:05:08 -0700

    123

---
+++ lab4/js/lab.js
@@ -0,0 +1,17 @@
+
+ //Summary.
+ //BlackJack Game.
+ //Description. (use period)
+ //Making use of values to create a simulator for populer cardgame, BlackJack.
+ //@link   https://chenzibo0104.github.io/art101/lab4
+ //@file   This files defines the class(values).
+ //@author Zibo Chen, Stephen Wei.
+ //@since  04.18.2022
+
+ //1. Refresh the data and start from (3) if any line is printed.
+ //2. Print "You Win" when SumValue == 21
+ //3. Print "You Lose" when SumValue > 21
+ //4.Giving the player 2 random values, each between 1-10
+ //5. Having the option for the player to chose rather to get another value or ///not, go to (6) if yes, and (7) if not.
+ //6. Caculate the sum of values the player holds, if the sum is -< 20, repeat (5).
+ //7. Pick a randomvalue between 2-20, if  < SumValue, print "You Win"; if == SumValue, print "Tie".
